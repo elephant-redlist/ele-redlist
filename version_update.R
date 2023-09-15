ec069b3bbe4e092adbd3831bbe0ddd12086d6153
Charles <cescapecs@gmail.com>
2023-09-15 19:53:30 +0200

    Update version_update.R

--- version_update.R
+++ version_update.R
@@ -1,10 +1,8 @@
 # Get Version and date
 VERSION <- scan('DESCRIPTION',what = character(),skip = 3,nlines = 1)[2]
+VERSION <- nzbd::versionUpdate(VERSION, "0.0.1")
 DATE    <- Sys.Date()
 TIME    <- Sys.time()
-
-# increment development version
-VERSION <- paste(substr(VERSION, start = 1, stop = 4), as.numeric(substr(VERSION, start = 5, stop = 10)) + 1, sep = '')
 
 # update DESCRIPTION
 DESCRIPTION    <- readLines('DESCRIPTION')
@@ -17,6 +15,6 @@ rm(DESCRIPTION)
 filename <- "R/zzz.R"
 cat(".onAttach <- function(libname, pkgname)\n", file = filename)
 cat("{\n", file = filename, append = TRUE)
-cat(paste("    packageStartupMessage(\"red version ", VERSION, " (", TIME, ")\")\n", sep = ""), file = filename, append = TRUE)
+cat(paste("    packageStartupMessage(\"red version ", VERSION, " (", DATE, ")\")\n", sep = ""), file = filename, append = TRUE)
 cat("}\n", file = filename, append = TRUE)
 rm(filename)
